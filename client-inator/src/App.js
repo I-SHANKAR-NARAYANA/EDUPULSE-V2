83e14ebd036b70a6aaa20dd30216d7f13e0ece7c
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2023-11-12 14:39:32 +0530

    First command

---
+++ client-inator/src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Redirect,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Addcourse from "./pages/Addcourse";
+import THome from "./pages/THome";
+import SHome from "./pages/SHome";
+import Allcourses from "./pages/Allcourses";
+import Playlist from "./pages/Playlist";
+import Video from "./pages/Video";
+import About from "./pages/About";
+import Teachers from "./pages/Teachers";
+import Contact from "./pages/Contact";
+import Allhome from "./pages/allhome";
+import PrivateRoute from "./PrivateRoute";
+
+const App = () => {
+  return (
+    <div id="root">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Allhome />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route
+            path="/register"
+            element={<PrivateRoute element={<Register />} allowedRoles={""} />}
+          />
+          <Route
+            path="/login"
+            element={<PrivateRoute element={<Login />} allowedRoles={""} />}
+          />
+          <Route
+            path="/thome"
+            element={
+              <PrivateRoute element={<THome />} allowedRoles={"teacher"} />
+            }
+          />
+          <Route
+            path="/addCourse"
+            element={
+              <PrivateRoute element={<Addcourse />} allowedRoles={"teacher"} />
+            }
+          />
+          <Route
+            path="/shome"
+            element={
+              <PrivateRoute element={<SHome />} allowedRoles={"student"} />
+            }
+          />
+          <Route
+            path="/video"
+            element={
+              <PrivateRoute element={<Video />} allowedRoles={"video"} />
+            }
+          />
+          <Route
+            path="/playlist"
+            element={
+              <PrivateRoute element={<Playlist />} allowedRoles={"video"} />
+            }
+          />
+          <Route path="/allcourses" element={<Allcourses />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/teachers" element={<Teachers />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
